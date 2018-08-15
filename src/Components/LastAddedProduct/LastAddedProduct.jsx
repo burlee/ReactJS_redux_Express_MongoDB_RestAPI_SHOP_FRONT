21cc5eecb2c7090400970ad37d07c1f3fd612126
Seweryn Kimla <sewkim@vp.pl>
2018-08-15 21:20:07 +0200

    Switch display last added product

--- src/Components/LastAddedProduct/LastAddedProduct.jsx
+++ src/Components/LastAddedProduct/LastAddedProduct.jsx
@@ -1,34 +1,77 @@
 import React, { Component } from 'react'
 import classes from './LastAddedProduct.css'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+import axios from 'axios'
 
 class LastAddedProduct extends Component {
-  render() {
+    state = {
+        nextProductVal: 3,
+        productPrevious: 0,
+        backBtnDisabled: true,
+        nextBtnDiabled: false,
+        productsArrayLength: 0
+    }
+    
+    
+    componentDidMount(){
+        axios.get('http://localhost:3000/offers/')
+            .then( response => {
+                this.setState({productsArrayLength: response.data.count})
+            })
+    
+    }
 
-    const arrayLength = this.props.allProducts.allProducts.length;
-    const arrayReverse = arrayLength - 3;
-    console.log( arrayReverse )
-    console.log( arrayLength )
+    nextProduct = () => {
+        const oldproductPrevious = this.state.productPrevious;
+        this.setState({
+            productPrevious: oldproductPrevious  + 1
+        })
+        if(this.state.productPrevious >= 0){
+            this.setState({backBtnDisabled: false})
+        }
+        if(this.state.productsArrayLength - 4 === this.state.productPrevious){
+            this.setState({nextBtnDiabled: true})
+        }
+    }
 
-    let lastAddedProduct = this.props.allProducts.allProducts.slice(arrayReverse, arrayLength).map( product => {
+    backProduct = () => {
+        const oldproductPrevious = this.state.productPrevious;
+        this.setState({
+            productPrevious: oldproductPrevious  - 1
+        })
+        if(this.state.productPrevious <= 1){
+            this.setState({backBtnDisabled: true})
+        }
+        if(this.state.productsArrayLength - 4 < this.state.productPrevious){
+            this.setState({nextBtnDiabled: false})
+        }
+    }
+
+    render() {
+
+        const arrayLength = this.props.allProducts.allProducts.length - this.state.productPrevious;
+        const arrayReverse = arrayLength - 3;
+
+        let lastAddedProduct = this.props.allProducts.allProducts.slice(arrayReverse, arrayLength).map(product => {
+            return (
+                <div key={product.id} className={classes.productDisplay}>
+                    <span>{product.productName}</span>
+                    <img src={product.productImgUrl} alt={product.productName} />
+                    <span>{product.productPrice} PLN</span>
+                </div>)
+        })
+
         return (
-        <div key={product.id} className={classes.productDisplay}>
-            <span>{product.productName}</span>
-            <img src={product.productImgUrl} alt={product.productName}/>
-            <span>{product.productPrice} PLN</span>
-        </div>)
-    })
-    return (
-      <div className={classes.LastAddedProductContainer}>
-        <h1>Ostatnio dodane produkty</h1>
-        <div className={classes.productContainer}>
-            <button>&gt;</button>
-                {lastAddedProduct}
-            <button>&lt;</button>
-        </div>
-      </div>
-    )
-  }
+            <div className={classes.LastAddedProductContainer}>
+                <h1>Ostatnio dodane produkty</h1>
+                <div className={classes.productContainer}>
+                    <button disabled={this.state.nextBtnDiabled} onClick={this.nextProduct}>&gt;</button>
+                    {lastAddedProduct}
+                    <button disabled={this.state.backBtnDisabled} onClick={this.backProduct}>&lt;</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => ({
